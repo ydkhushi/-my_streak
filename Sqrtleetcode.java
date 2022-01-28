0227ce9a174631d1279dd98417117ef596ca8f69
Khushi Yadav <90633891+ydkhushi@users.noreply.github.com>
2022-01-29 02:31:25 +0530

    Add files via upload

---
+++ Sqrtleetcode.java
@@ -0,0 +1,12 @@
+//https://leetcode.com/problems/sqrtx/
+package leetPlusOne;
+
+class Solution
+{
+    public int mySqrt(int x) 
+    {
+        double a=Math.sqrt(x);
+        return (int)a;
+        
+    }
+}
